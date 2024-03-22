39481d3fc5780c9f8e7a5d5da42d8acb24b275a2
Hidde Manz <hiddemanz@gmail.com>
2024-03-22 11:33:20 +0100

    basic themplate html gemaakt

--- www/database.php
+++ www/database.php
@@ -1,3 +1,4 @@
 <?php
 
 //maak een database connectie
+
--- www/index.php
+++ www/index.php
@@ -1,1 +1,29 @@
 
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <link href="" rel="stylesheet" />
+    <title>RECEPTENBOEK</title>
+</head>
+ 
+<body>
+    <header>
+       
+    </header>
+    <div class="background-image">
+        
+        </div>
+        <div class="boven-right-image">
+            
+        </div>
+    </div>
+ 
+    <footer>
+        <div class="onderkant">
+          
+        </div>
+    </footer>
+</body>
+ 
+</html>
